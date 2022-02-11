6630fa8bb33a07a484ca1404ab33c90c1aa94f89
GhostyO17 <oscar.castro9617@gmail.com>
2022-02-11 01:48:12 -0500

    changed format specifier

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,10 +4,10 @@
  *Return: Always 0 (Success)
  */
 int main(void)
-{printf("Size of a char: %lu byte(s)\n", sizeof(char));
-printf("Size of an int: %lu byte(s)\n", sizeof(int));
-printf("Size of a long int is: %lu byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(longint*));
-printf("Size of a float: %lu byte(s)\n", sizeof(float));
+{printf("Size of a char: %ld byte(s)\n", sizeof(char));
+printf("Size of an int: %ld byte(s)\n", sizeof(int));
+printf("Size of a long int is: %ld byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(unsigned long int));
+printf("Size of a float: %ld byte(s)\n", sizeof(float));
 return (0);
 }
