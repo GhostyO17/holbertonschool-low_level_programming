8a3f2b22e679e4a3cdf8ac01802fcce458a4848f
GhostyO17 <oscar.castro9617@gmail.com>
2022-02-11 01:18:26 -0500

    working exercise adds byte(s)

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,10 +4,10 @@
  *Return: Always 0 (Success)
  */
 int main(void)
-{printf ("size of a char: %lu\n", sizeof(char));
-  printf ("size of an int: %lu\n", sizeof(int));
-  printf ("size of a long int is: %lu\n", sizeof(long int));
-  printf ("Size of a long long int: %lu\n", sizeof(long int *));
-  printf ("Size of a float: %lu\n", sizeof(float));
+{printf("size of a char: %lu byte(s) \n", sizeof(char));
+printf("size of an int: %lu byte(s) \n", sizeof(int));
+printf("size of a long int is: %lu byte(s) \n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s) \n", sizeof(long int *));
+printf("Size of a float: %lu byte(s) \n", sizeof(float));
 return (0);
 }
