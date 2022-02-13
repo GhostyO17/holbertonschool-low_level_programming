5007b70ff0ac87fe7b3d6e3113d505bed4019518
GhostyO17 <oscar.castro9617@gmail.com>
2022-02-13 18:47:19 -0500

    Adds working exercise 4-Betty OK-GCC OK

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int nume = 0;
+while (nume < 10)
+{
+printf("%d", nume);
+nume++;
+}
+putchar('\n');
+return (0);
+}
+
