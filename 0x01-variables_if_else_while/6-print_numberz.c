f777108c608345d5a4c75cc956faf147015a69df
GhostyO17 <oscar.castro9617@gmail.com>
2022-02-13 19:01:04 -0500

    Exercise 6- Betty Ok- GCC ok, doesn't print numbers, testing

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int nume = 0;
+while (nume < 10)
+{
+putchar(nume);
+nume++;
+}
+putchar('\n');
+return (0);
+}
