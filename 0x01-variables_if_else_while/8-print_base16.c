3734d761967a57b472b09eea996db1fbdaddf774
GhostyO17 <oscar.castro9617@gmail.com>
2022-02-14 00:43:59 -0500

    adds working exercise 8-Betty OK-GCC OK-prints with ':' between num and hex, test

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int num = 0;
+char hex = 'a';
+while (num <= 10)
+{putchar(num + '0');
+num++;
+}
+while (hex <= 'f')
+{putchar(hex);
+hex++;
+}
+putchar('\n');
+return (0);
+}
+
