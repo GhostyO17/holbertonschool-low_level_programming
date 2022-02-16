dcd801fa2f80de17f2c2e59820a95fb5f462bbb2
GhostyO17 <oscar.castro9617@gmail.com>
2022-02-15 23:52:44 -0500

    adds exercise 7 wrong output, pushing for test

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a given number
+ * Return: last digit
+ * @i: input value
+ */
+
+int print_last_digit(int i){
+return (i % 10);
+}
+
