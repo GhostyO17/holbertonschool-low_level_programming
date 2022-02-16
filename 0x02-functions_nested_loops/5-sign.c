d673e474b81d74601f9dcd66f3543a17fddde4f3
GhostyO17 <oscar.castro9617@gmail.com>
2022-02-15 21:36:38 -0500

    commits proper .c file and removes exe file

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sing of n
+ * @n: character to read
+ * Return: 1 if n is positive, 0 if it's 0 and -1 if it is negative
+ */
+
+
+int print_sign(int n)
+{
+
+int num, ret;
+
+num = n;
+ret = 0;
+
+if (num > 0)
+{
+_putchar('+');
+ret = 1;
+}
+else if (num < 0)
+{
+  _putchar('-');
+ret = 0;
+}
+if (num == 0)
+{
+_putchar('0');
+ret = -1;
+}
+return(ret);
+}
