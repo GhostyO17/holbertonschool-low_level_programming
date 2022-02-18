0aa3ecdd05ead67b8af952bda996d5d148fba623
GhostyO17 <oscar.castro9617@gmail.com>
2022-02-17 22:33:53 -0500

    adds exercise 3 Betty OK-GCC ok- updates main.h

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_numbers - print numbers from 0 to 9
+* Return: 0
+*/
+
+void print_numbers(void)
+
+{
+char x = '0';
+while (x <= '9')
+{
+_putchar(x);
+x++;
+}
+_putchar ('\n');
+}
