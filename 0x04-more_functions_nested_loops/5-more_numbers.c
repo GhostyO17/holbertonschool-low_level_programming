b0a07aba47013f359bd0e1d25c978ae24a976182
GhostyO17 <oscar.castro9617@gmail.com>
2022-02-17 23:32:01 -0500

    Adds working exercise 5 and updated main.h

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints digits from 0 to 14 ten times
+ */
+
+void more_numbers(void)
+{
+int z;
+char d;
+for (z = 0; z < 10; z++)
+{
+for (d = 0; d <= 14; d++)
+{
+if (d > 9)
+_putchar((d / 10) + '0');
+_putchar((d % 10) + '0');
+}
+_putchar('\n');
+}
+return;
+}
