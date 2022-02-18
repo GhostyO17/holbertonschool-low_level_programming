bf6c526fa85c222fab17a788f7e6a99383514b84
GhostyO17 <oscar.castro9617@gmail.com>
2022-02-17 23:41:11 -0500

    adds exercise 6 for test and main.h updated

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - prints a dash 
+ * @n: input
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+char i;
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
