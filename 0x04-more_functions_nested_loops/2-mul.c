25eb1900b428c64d79c5aed562960154fd1d36bb
GhostyO17 <oscar.castro9617@gmail.com>
2022-02-17 22:30:22 -0500

    adds exercise 2 Betty OK-GCC ok- updates main.h

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* mul - function that multiplies two numbers
+* @a: number to operate with
+* @b: number to operate with
+* Return: 1 or 0
+*/
+
+int mul(int a, int b)
+{
+return (a * b);
+}
