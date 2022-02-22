29e937fb1c4cce5bd0d4b61995e98eb55b42ecb3
GhostyO17 <oscar.castro9617@gmail.com>
2022-02-21 23:09:05 -0500

    commits exercise 2 using strlen, gcc failed, adds updated main.h

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: string lenght.
+ */
+int _strlen(char *s)
+{
+strlen (char s[0]);
+return(_strlen);
+}
+
