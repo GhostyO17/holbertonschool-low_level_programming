5109b52c2911a64e3e32b50386d1783b425111bb
GhostyO17 <oscar.castro9617@gmail.com>
2022-02-21 21:06:40 -0500

    adds exercise 0 with betty ok

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * reset_to_98 - changes de variable value using pointers to 98 
- *
+ * reset_to_98 - changes de variable value using pointers to 98
+ * @n: variable to convert
  * Return: void.
  */
 void reset_to_98(int *n)
 {
-  *n = 98;
-    return;
+*n = 98;
+return;
 }
