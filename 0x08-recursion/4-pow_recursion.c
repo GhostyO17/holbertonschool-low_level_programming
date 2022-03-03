870c5e81950fd96e12b5ff3ac95ab3fc5ac3e152
GhostyO17 <oscar.castro9617@gmail.com>
2022-03-02 23:57:54 -0500

    Adds proper description

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * _pow_recursion - prints a string backwards
- * @s: inout string
+ * _pow_recursion - raises x to he power of y
+ * @x: number
+ * @y: raise to
+ * Return: x to the power of y
  */
 
 int _pow_recursion(int x, int y)
-
 {
 if (y < 0)
 return (-1);
