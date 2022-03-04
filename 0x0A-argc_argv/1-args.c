82419ceab39f76b00b9e476605b76caa78a09b62
GhostyO17 <oscar.castro9617@gmail.com>
2022-03-04 13:03:33 -0500

    adds working exercises 1 and 2

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*@argc: counter of flags
+*@argv: array that contains the name of the file
+* Return: 0
+**/
+
+int main(int argc, char *argv[])
+{
+argv = *arvg;
+printf("%d\n", argc);
+return (0);
+}
