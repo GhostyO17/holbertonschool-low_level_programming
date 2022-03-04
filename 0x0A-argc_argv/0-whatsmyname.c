80bcd93d673d08dd7e876b679ab8ad298dcb14db
GhostyO17 <oscar.castro9617@gmail.com>
2022-03-04 11:52:18 -0500

    Adds directory 0x0A with working exercise 0 betty ok gcc ok and README

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*@argc: counter of flags
+*@argv: array that contains the name of the file
+* Return: 0
+**/
+
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+argc++;
+}
