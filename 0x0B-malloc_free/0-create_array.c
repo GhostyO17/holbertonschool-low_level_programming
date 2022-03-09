571a51fb5bf0b732304d2f91e7a0676912318760
GhostyO17 <oscar.castro9617@gmail.com>
2022-03-08 22:30:44 -0500

    Adds directory 0x0B with Readme and exercises

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - creates an array of chars that begins with c
+ * @size: size of the array
+ * @c: char which initializes the array
+ * Description: creates an array of chars
+ * Return: pointer to array, NULL if fail
+ *
+ **/
+char *create_array(unsigned int size, char c)
+{
+char *str;
+unsigned int i;
+str = malloc(sizeof(char) * size);
+if (size == 0 || str == NULL)
+return (NULL);
+for (i = 0; i < size; i++)
+str[i] = c;
+return (str);
+}
