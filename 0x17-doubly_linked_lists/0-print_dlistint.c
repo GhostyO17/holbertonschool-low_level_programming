fe9f5a3029bb40c8a83472b73fadb1acdc9267b6
GhostyO17 <oscar.castro9617@gmail.com>
2022-05-12 12:51:09 -0500

    Output test betty no checkes gcc ok

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,1 +1,21 @@
-s
+#include "lists.h"
+
+/**
+ * print_dlistint - function that creates a file.
+ * @
+ * @
+ * Return: 1 on success, -1 on failure
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+int i = 0;
+    if (h == NULL)
+        return(0);
+for (;h != NULL; i++)
+{
+printf("%d\n", i);
+h = h->next;
+}
+return(i);
+}
