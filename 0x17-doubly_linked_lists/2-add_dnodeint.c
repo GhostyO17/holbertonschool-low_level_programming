c9021456f45784ee0c5e625062fc11f0d7fa6674
GhostyO17 <oscar.castro9617@gmail.com>
2022-05-12 15:38:14 -0500

    Adds exercis 2 betty ok

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - returns the number of nodes
+ * @head: head element
+ * @n: int n
+ * Return: number of nodes
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+dlistint_t *new_node;
+new_node = malloc(sizeof(dlistint_t));
+if (new_node == NULL)
+return (NULL);
+new_node->n = n;
+new_node->prev = NULL;
+new_node->next = NULL;
+if (*head == NULL)
+{
+*head = new_node;
+return (new_node);
+}
+else
+{
+(*head)->prev = new_node;
+new_node->next = *head;
+*head = new_node;
+return (new_node);
+}
+}
