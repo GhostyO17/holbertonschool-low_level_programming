9ebd2a1272cb327db14173e84781c6a2b4902722
GhostyO17 <oscar.castro9617@gmail.com>
2022-05-12 13:03:08 -0500

    Adds exercise betty ok

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,21 +1,20 @@
 #include "lists.h"
 
 /**
- * print_dlistint - function that creates a file.
- * @
- * @
- * Return: 1 on success, -1 on failure
+ * print_dlistint - prints all the nodes of the list
+ * @h: elements that belong to dlistint
+ * Return: number of nodes
  */
 
 size_t print_dlistint(const dlistint_t *h)
 {
 int i = 0;
-    if (h == NULL)
-        return(0);
-for (;h != NULL; i++)
+if (h == NULL)
+return (0);
+for (; h != NULL; i++)
 {
 printf("%d\n", i);
 h = h->next;
 }
-return(i);
+return (i);
 }
