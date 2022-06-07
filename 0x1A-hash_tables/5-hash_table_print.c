f655b81b239e3ab565c4f3ae97099e3a9112643a
GhostyO17 <oscar.castro9617@gmail.com>
2022-06-07 11:47:52 -0500

    Corrects exercise 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -10,22 +10,22 @@ unsigned int i = 0;
 unsigned long int idx;
 hash_node_t *temp = NULL;
 
-	if (ht == NULL)
-		return;
+if (ht == NULL)
+return;
 
 printf("{");
 
 for (idx = 0; idx < ht->size; idx++)
-	{
-	while (temp != NULL)
-	{
-	if (i == 1)
-		printf(", ");
-		printf("'%s': '%s'", temp->key, temp->value);
-		i = 1;
-	temp = temp->next;
-	}
-		temp = ht->array[idx];
-	}
+{
+while (temp != NULL)
+{
+if (i == 1)
+printf(", ");
+printf("'%s': '%s'", temp->key, temp->value);
+i = 1;
+temp = temp->next;
+}
+temp = ht->array[idx];
+}
 printf("}\n");
 }
